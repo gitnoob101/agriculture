a4f6a14f0b5c609213a7a347c24991954d3fd450
Shubham Verma <vermaprince2004@gmail.com>
2025-08-18 18:54:22 +0530

    upd config cors

--- main.py
+++ main.py
@@ -42,9 +42,11 @@ app = FastAPI(
     version="1.1.0",
 )
 
+origins = ["*","http://localhost:5173"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
